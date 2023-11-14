aaf38c75e243c736a114bcd210660a8724ceac36
moguy93 <guymorgan@live.fr>
2023-11-14 15:00:15 +0100

    add correction

--- todo/src/app/components/logout/logout.component.ts
+++ todo/src/app/components/logout/logout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.scss']
+  styleUrls: ['./logout.component.scss'],
 })
 export class LogoutComponent {
-
+  constructor(public auth: AuthService) {}
 }
